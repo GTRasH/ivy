400ad4a9186464273f40de6eaa34f13a9b8613f5
GTRasH <Schnorrimaster@web.de>
2015-12-04 23:03:53 +0100

    replaced placeholder with data, shows data passed from DashboardController@index

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,32 @@
 
 @section('content')
 
-    <div class="title">Dashboard</div>
-    {{}}
+    <h1 class="page-header">Dashboard</h1>
+
+    <h2 class="sub-header">Latest YT Data</h2>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Channel-ID</th>
+                <th>Title</th>
+                <th>AllViews</th>
+                <th>Subscribers</th>
+                <th>Comments</th>
+                <th>Videos</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>{{$data['id']}}</td>
+                <td>{{$data['title']}}</td>
+                <td>{{$data['viewcount']}}</td>
+                <td>{{$data['subscribers']}}</td>
+                <td>{{$data['comments']}}</td>
+                <td>{{$data['videos']}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 
 @endsection
