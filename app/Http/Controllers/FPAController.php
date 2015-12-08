33910cd88dba208c3ff050993b038efc48faf183
GTRasH <Schnorrimaster@web.de>
2015-12-08 19:04:01 +0100

    Created stub functions for FPA

--- app/Http/Controllers/FPAController.php
+++ app/Http/Controllers/FPAController.php
@@ -16,7 +16,7 @@ class FPAController extends Controller
      */
     public function index()
     {
-        //
+        // return FPA Menu Main
     }
 
     /**
@@ -24,9 +24,9 @@ class FPAController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function leman()
     {
-        //
+        // return LAMEMAN
     }
 
     /**
@@ -35,53 +35,9 @@ class FPAController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function shorts()
     {
-        //
+        // RK Shorts management
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
