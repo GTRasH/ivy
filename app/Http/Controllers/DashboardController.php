f526ee7874cfcce8d9a9da8e5acdca83180ce13d
GTRasH <Schnorrimaster@web.de>
2015-12-08 08:27:45 +0100

    reformatted

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,18 +15,26 @@ class DashboardController extends Controller
     public function index() {
 
         $channel = Youtube::getChannelById('UCZBzhwB0w9M686PF7Z649Nw');
-//dd($channel->statistics->viewCount);
-    $data=array(
-            'id'            =>  $channel->id,
-            'title'         =>  $channel->snippet->title,
-            'description'   =>  $channel->snippet->description,
-            'viewcount'     =>  $channel->statistics->viewCount,
-            'subscribers'   =>  $channel->statistics->subscriberCount,
-            'comments'      =>  $channel->statistics->commentCount,
-            'videos'        =>  $channel->statistics->videoCount
-            );
-        return view('dashboard')->with('data',$data);
 
+        if (!empty($channel->id)) {
+
+
+//dd($channel->statistics->viewCount);
+            $data = array(
+                'id' => $channel->id,
+                'title' => $channel->snippet->title,
+                'description' => $channel->snippet->description,
+                'viewcount' => $channel->statistics->viewCount,
+                'subscribers' => $channel->statistics->subscriberCount,
+                'comments' => $channel->statistics->commentCount,
+                'videos' => $channel->statistics->videoCount
+            );
+
+            return view('dashboard')->with('data',$data);
+        }
+        else {
+            return 'sorry, connecting to youtube failed';
+        }
     }
 
     public function videos() {
